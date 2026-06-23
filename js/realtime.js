f5254c1a201c37336fa4e0a903d64972197d2d92
Zakii <zakii@360-search.com>
2026-06-22 20:38:22 -0400

    Delete js directory

--- js/realtime.js
+++
@@ -1,7 +0,0 @@
-
-function setupRealtime(){
- sb.channel('chat-live')
- .on('postgres_changes',{event:'INSERT',schema:'public',table:'chat_messages'},
- payload=>addMessage(payload.new))
- .subscribe();
-}
